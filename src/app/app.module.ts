4e99872f32dd9057246c19a02aa28ab952610d80
Machuka Joy <machukajoy@gmail.com>
2022-04-19 12:01:07 +0300

    ft(service): add service to generate data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { SearchComponent } from './Components/search/search.component';
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
